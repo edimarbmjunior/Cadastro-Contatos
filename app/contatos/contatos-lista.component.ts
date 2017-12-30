ff8ae6c15626cb9f0883e1b6ac9e0c66a6331697
Edimar <edimarbmjunior@gmail.com>
2017-12-30 21:37:30 -0200

    ajustes de rotas

--- app/contatos/contatos-lista.component.ts
+++ app/contatos/contatos-lista.component.ts
@@ -1,16 +1,23 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Contato } from './contato.model';
-import { CONTATOS } from './contatos-mock';
-
+import { ContatoService } from './contato.service';
 
 @Component({
     moduleId: module.id,
     selector: 'contatos-lista',
     templateUrl: 'contatos-lista.component.html'
 })
-export class ContatosListaComponent{
+export class ContatosListaComponent implements OnInit{
     
-    contatos: Contato[] = CONTATOS;
+    contatos: Contato[];
+    constructor(private contatoService: ContatoService){}
+
+    ngOnInit(): void{
+        //this.contatos = this.contatoService.getContatos(); -> sem promises
+        this.contatoService.getContatos().then((contatos: Contato[])=> {
+            this.contatos = contatos;
+        }).catch(err=> console.log(err));
+    }
 
 }
