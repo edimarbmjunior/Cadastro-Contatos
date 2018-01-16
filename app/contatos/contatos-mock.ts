bd774b3cfa06599957c48dab637de303ef5b7511
Edimar <edimarbmjunior@gmail.com>
2018-01-15 22:04:02 -0200

    ...

--- app/contatos/contatos-mock.ts
+++ app/contatos/contatos-mock.ts
@@ -6,4 +6,4 @@ export const CONTATOS: Contato[] = [
     {id:3, nome:'Uva', email: 'uva@email', telefone: '(00) 0000-0002'},
     {id:4, nome:'Limão', email: 'limao@email', telefone: '(00) 0000-0003'},
     {id:5, nome:'Mariana', email: 'mariana@email', telefone: '(00) 0000-0004'}
-]
+];
